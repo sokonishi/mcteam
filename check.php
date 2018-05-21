560920f7f3887f77ed82b796406f0e58e735c485
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-05-22 05:41:23 +0800

    シンプルな登録画面

---
+++ check.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>missyou</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-8 col-xs-offset-2 thumbnail">
+          <h2 class="text-center content_header">アカウント情報確認</h2>
+          <div class="row">
+            <div class="col-xs-4">
+              <img src=".img" class="img-responsive img-tumbnail">
+            </div>
+            <div class="col-xs-8">
+              <div>
+                <span>名前</span>
+                <p class="lead">ユーザー名が入ります。</p>
+              </div>
+              <div>
+                <span>メールアドレス</span>
+                <p class="lead">メールアドレスが入ります。</p>
+              </div>
+              <div>
+                <span>パスワード</span>
+                <p class="lead">●●●●●●●●●●</p>
+              </div>
+              <a href="signup.php" class="btn" onclick="history.back()">&laquo;&nbsp;戻る</a>
+              <input type="submit" class="btn btn-primary" value="登録">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <script src="../assets/js/jquery-3.1.1.js"></script>
+    <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>missyou</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/elohssa.css">
+</head>
+<body>
+
+
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2">
+                <h2 class="text-center content_header">新規登録</h2>
+                <form>
+                    <div class="form-group">
+                        <label>ユーザー名</label>
+                        <input type="text" name="#" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>メールアドレス</label>
+                        <input type="email" name="#" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>パスワード</label>
+                      <input type="pasward" name="#" class="form-control">
+                    </div>
+                    <div class="form-group">
+                      <label>プロフィール画像</label>
+                      <input type="file" name="#" accept="image/*">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="確認">
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="../assets/js/jquery-3.1.1.js"></script>
+    <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+</body>
+</html>
