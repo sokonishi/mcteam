5b01a3b0a3bdb89fdc3f9675091d7ea80482f29e
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-05-22 22:24:56 +0800

    timeline.php

--- timeline.php
+++ timeline.php
@@ -1,17 +1,125 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="utf-8">
-    <title>missyou</title>
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+  <meta charset="utf-8">
+  <title>missyou</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/elohssa.css">
 </head>
 <body>
+  <nav class="navbar navbar-default navbar-fixed-top custom-navbar">
+    <div class="container">
+<!--       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="  navbar">
+       <span class="sr-only">Toggle navigation</span>
+       <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+     </button> -->
+     <div class="navbar-header">
+       <a class="navbar-brand" href="#"><img src="img/missyou_logo.png" style="width: 20%"></a>
+     </div>
+   <div id="navbar" class="navbar-collapse collapse">
+     <ul class="nav navbar-nav col-xs-offset-10">
+       <li class="active"><a href="#">Home</a></li>
+       <li><a href="#about">About</a></li>
+       <li><a href="#contact">Contact</a></li>
+       <li class="dropdown">
+         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+         <ul class="dropdown-menu" role="menu">
+           <li><a href="#">Action</a></li>
+           <li><a href="#">Another action</a></li>
+           <li><a href="#">Something else here</a></li>
+           <li class="divider"></li>
+           <li class="dropdown-header">Nav header</li>
+           <li><a href="#">Separated link</a></li>
+           <li><a href="#">One more separated link</a></li>
+         </ul>
+       </li>
+     </ul>
+     <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
+
+  <div class="background">
+    <div class="container">
+      <div class="row col-xs-offset-10">
+        <div class="you_img">
+          <img src="img/copy.png" style="width: 50%">
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div class="row">
+
+<div class="container">
+  <div class="row">
+      <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+        <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+        <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+  </div>
+</div>
+
+      </div>
+    </div>
+  </div>
 
 
-    <script src="../assets/js/jquery-3.1.1.js"></script>
-    <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
-    <script src="../assets/js/bootstrap.js"></script>
+  <script src="assets/js/jquery-3.1.1.js"></script>
+  <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
 </body>
 </html>
