7addb0f6ff8ab5178b3a72054d3d144218aacf82
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-05-24 11:23:47 +0800

    no message

--- timeline.php
+++ timeline.php
@@ -3,7 +3,12 @@
 <head>
   <meta charset="utf-8">
   <title>missyou</title>
+
+
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+
   <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
   <link rel="stylesheet" type="text/css" href="assets/css/elohssa.css">
@@ -11,22 +16,25 @@
 <body>
   <nav class="navbar navbar-default navbar-fixed-top custom-navbar">
     <div class="container">
-<!--       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="  navbar">
+<!--       <カートに入れる type="カートに入れる" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="  navbar">
        <span class="sr-only">Toggle navigation</span>
        <span class="icon-bar"></span>
        <span class="icon-bar"></span>
        <span class="icon-bar"></span>
-     </button> -->
+     </カートに入れる> -->
      <div class="navbar-header">
        <a class="navbar-brand" href="#"><img src="img/missyou_logo.png" style="width: 20%"></a>
      </div>
-   <div id="navbar" class="navbar-collapse collapse">
-     <ul class="nav navbar-nav col-xs-offset-10">
+  <!--  <div id="navbar" class="navbar-collapse collapse"> -->
+   <div class="col-xs-offset-10">
+     <img src="img/menu_bar.png" style="width: 20%">
+   </div>
+     <!-- <ul class="nav navbar-nav col-xs-offset-10">
        <li class="active"><a href="#">Home</a></li>
        <li><a href="#about">About</a></li>
        <li><a href="#contact">Contact</a></li>
        <li class="dropdown">
-         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="カートに入れる" aria-expanded="false">Dropdown <span class="caret"></span></a>
          <ul class="dropdown-menu" role="menu">
            <li><a href="#">Action</a></li>
            <li><a href="#">Another action</a></li>
@@ -37,41 +45,40 @@
            <li><a href="#">One more separated link</a></li>
          </ul>
        </li>
-     </ul>
-     <ul class="nav navbar-nav navbar-right">
+     </ul> -->
+     <!-- <ul class="nav navbar-nav navbar-right">
               <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
+            </ul> -->
+          <!-- </div> --><!--/.nav-collapse -->
         </div>
       </nav>
 
   <div class="background">
     <div class="container">
       <div class="row col-xs-offset-10">
-        <div class="you_img">
+        <div class="copy_img top">
           <img src="img/copy.png" style="width: 50%">
         </div>
       </div>
     </div>
-    
-    <div class="container">
-      <div class="row">
 
 <div class="container">
+  <!-- row1 -->
   <div class="row">
       <div class="col-md-4">
     <!--Card-->
             <div class="card">
                 <!--Card image-->
-                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+                <img class="img-fluid" src="img/1.png" alt="Card image cap">
             
                 <!--Card content-->
                 <div class="card-body">
                     <!--Title-->
                     <h4 class="card-title">Card title</h4>
                     <!--Text-->
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Button</a>
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり、翼は小さくなって数メートル程しか飛ぶことができなくなった。また、体型も太ったもの、直立して歩くものなど色々変化した。</p>
+                    <h6 class="cost">2,000,000円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
                 </div>
             </div>
         <!--/.Card-->
@@ -80,15 +87,16 @@
     <!--Card-->
             <div class="card">
                 <!--Card image-->
-                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+                <img class="img-fluid" src="img/2.png" alt="Card image cap">
             
                 <!--Card content-->
                 <div class="card-body">
                     <!--Title-->
                     <h4 class="card-title">Card title</h4>
                     <!--Text-->
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Button</a>
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり。</p>
+                    <h6 class="cost">200円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
                 </div>
             </div>
         <!--/.Card-->
@@ -97,24 +105,143 @@
     <!--Card-->
             <div class="card">
                 <!--Card image-->
-                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+                <img class="img-fluid" src="img/3.png" alt="Card image cap">
             
                 <!--Card content-->
                 <div class="card-body">
                     <!--Title-->
                     <h4 class="card-title">Card title</h4>
                     <!--Text-->
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Button</a>
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり、翼は小さくなって数メートル程しか飛ぶことができなくなった。</p>
+                    <h6 class="cost">8,000,000,000円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
                 </div>
             </div>
         <!--/.Card-->
         </div>
   </div>
+  <!-- row2 -->
+  <div class="row">
+      <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="img/3.png" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり、翼は小さくなって数メートル程しか飛ぶことができなくなった。また、体型も太ったもの、直立して歩くものなど色々変化した。</p>
+                    <h6 class="cost">2円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+        <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="img/1.png" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり、翼は小さくなって数メートル程しか飛ぶことができなくなった。</p>
+                    <h6 class="cost">200円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+        <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="img/2.png" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり、翼は小さくなって数メートル程しか飛ぶことができなくなった。また、体型も太ったもの、直立して歩くものなど色々変化した。</p>
+                    <h6 class="cost">100円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+  </div>
+  <!-- row3 -->
+  <div class="row">
+      <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="img/1.png" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり、翼は小さくなって数メートル程しか飛ぶことができなくなった。また、体型も太ったもの、直立して歩くものなど色々変化した。</p>
+                    <h6 class="cost">40,000円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+        <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="img/2.png" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり。</p>
+                    <h6 class="cost">4,000,000円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+        <div class="col-md-4">
+    <!--Card-->
+            <div class="card">
+                <!--Card image-->
+                <img class="img-fluid" src="img/3.png" alt="Card image cap">
+            
+                <!--Card content-->
+                <div class="card-body">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">野生のマガモを飼いならして家畜化するうち、個体や品種にもよるが、体が大きく重くなり、翼は小さくなって数メートル程しか飛ぶことができなくなった。</p>
+                    <h6 class="cost">4,000円</h6>
+                    <a href="#" class="btn btn-primary">カートに入れる</a>
+                </div>
+            </div>
+        <!--/.Card-->
+        </div>
+  </div>
+
 </div>
 
-      </div>
-    </div>
+
+
+  </div>
+
+
   </div>
 
 
