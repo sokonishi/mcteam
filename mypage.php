ad1970351be7b487a8ab5a965df0150486c9889b
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-05-28 12:30:53 +0800

    CSSとmypage.php

--- mypage.php
+++ mypage.php
@@ -3,8 +3,19 @@
 <head>
   <meta charset="utf-8">
   <title>missyou</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/elohssa.css">
 </head>
 <body>
+ <header>
+  <div class="header_logo">
+    <img src="img/missyou_logo.png" style="width: 20%">
+    <img src="img/menu_bar.png" style="width: 5%; float: right;">
+  </div>
+ </header>
+ 
 
 </body>
 </html>
