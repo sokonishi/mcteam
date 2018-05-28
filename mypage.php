73210d622c7e9c8c026c1bd5bb47dedc2301f1e8
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-05-29 07:47:03 +0800

    タイムラインとマイページの変更

--- mypage.php
+++ mypage.php
@@ -15,7 +15,132 @@
     <img src="img/menu_bar.png" style="width: 5%; float: right;">
   </div>
  </header>
- 
 
+  <div class="background">
+    <div class="container">
+      <div class="row space_mypage">
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="profile_card">
+          <div class="col-xs-3 col-md-2 col-lg-1">
+            <img src="img/item_img.png">
+          </div>
+            <h6>toshio0523</h6>
+            <p>投稿5件 フォロワー98人 フォロー中129件</p>
+            <h6>苗字　名前</h6>
+            <p>プロフィールが入ります。プロフィールが入ります。プロフィールが入ります。プロフィールが入ります。</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="card1">
+            <img src="img/item_img.png" style="width: 100%">
+            <h4>タイトル</h4>
+            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
+            <h4 class="cost">20,000円</h4>
+            <div class="purchase">
+              <button type="button" class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i> カートに入れる</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card1">
+            <img src="img/item_img.png" style="width: 100%">
+            <h4>タイトル</h4>
+            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
+            <h4 class="cost">20,000円</h4>
+            <div class="purchase">
+              <button type="button" class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i> カートに入れる</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card1">
+            <img src="img/item_img.png" style="width: 100%">
+            <h4>タイトル</h4>
+            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
+            <h4 class="cost">20,000円</h4>
+            <div class="purchase">
+              <button type="button" class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i> カートに入れる</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card1">
+            <img src="img/item_img.png" style="width: 100%">
+            <h4>タイトル</h4>
+            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
+            <h4 class="cost">20,000円</h4>
+            <div class="purchase">
+              <button type="button" class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i> カートに入れる</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="card1">
+            <img src="img/item_img.png" style="width: 100%">
+            <h4>タイトル</h4>
+            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
+            <h4 class="cost">20,000円</h4>
+            <div class="purchase">
+              <button type="button" class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i> カートに入れる</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card1">
+            <img src="img/item_img.png" style="width: 100%">
+            <h4>タイトル</h4>
+            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
+            <h4 class="cost">20,000円</h4>
+            <div class="purchase">
+              <button type="button" class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i> カートに入れる</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card1">
+            <img src="img/item_img.png" style="width: 100%">
+            <h4>タイトル</h4>
+            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
+            <h4 class="cost">20,000円</h4>
+            <div class="purchase">
+              <button type="button" class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i> カートに入れる</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="card1">
+            <img src="img/item_img.png" style="width: 100%">
+            <h4>タイトル</h4>
+            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
+            <h4 class="cost">20,000円</h4>
+            <div class="purchase">
+              <button type="button" class="btn btn-primary"><i class="fa fa-shopping-cart" aria-hidden="true"></i> カートに入れる</button>
+            </div>
+            <!-- /purchase -->
+          </div>
+          <!-- /card1 -->
+        </div>
+      </div>
+      <!-- /row -->
+    </div>
+    <!-- /container -->
+  </div>
+  <!-- /background -->
+  
+
+  <script src="assets/js/jquery-3.1.1.js"></script>
+  <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
 </body>
 </html>
