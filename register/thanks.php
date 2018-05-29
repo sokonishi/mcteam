8e868c65b6271a542c2a604b831a09aebe08f931
小西壮 <>
2018-05-30 06:25:50 +0800

    アカウント登録モック

--- register/thanks.php
+++ register/thanks.php
@@ -3,11 +3,21 @@
 <head>
     <meta charset="utf-8">
     <title>missyou</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/elohssa.css">
 </head>
 <body>
+
+  <header>
+    <div class="header_logo">
+      <img src="../img/missyou_logo.png" style="width: 20%">
+      <img src="../img/menu_bar.png" style="width: 5%; float: right;">
+    </div>
+  </header>
+
+  <div class="background">
     <div class="container">
       <div class="row">
         <div class="col-xs-8 col-xs-offset-2 thumbnail">
@@ -19,9 +29,10 @@
         </div>
       </div>
     </div>
+  </div>
 
-    <script src="../assets/js/jquery-3.1.1.js"></script>
-    <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
-    <script src="../assets/js/bootstrap.js"></script>
+  <script src="../assets/js/jquery-3.1.1.js"></script>
+  <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="../assets/js/bootstrap.js"></script>
 </body>
 </html>
