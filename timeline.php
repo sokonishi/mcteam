d617b7a7d5ec7be132467494b6330f9de079f595
LAPTOP-8F9BHPB1\masum <masumikawada914@gmail.com>
2018-05-31 11:10:19 +0800

    投稿表示機能、いいね機能

--- timeline.php
+++ timeline.php
@@ -1,3 +1,30 @@
+<?php 
+require('dbconnect.php');
+
+$sql = 'SELECT * FROM `feeds` ORDER BY `id` DESC';
+        $data = array();
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+
+        while (true) {
+            $record = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($record == false) {
+                break;
+            }
+
+            $like_sql = "SELECT COUNT(*) AS `like_cnt` FROM `likes` WHERE `feed_id` = ?";
+
+            $like_data = array($record["id"]);
+            $like_stmt = $dbh->prepare($like_sql);
+            $like_stmt->execute($like_data);
+
+            $like = $like_stmt->fetch(PDO::FETCH_ASSOC);
+            $record["like_cnt"] = $like["like_cnt"];
+
+            $feeds[] = $record;
+        }
+ ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -31,33 +58,24 @@
     </div>
 
     <div class="container">
+      <?php foreach($feeds as $feed){ ?>
       <div class="row">
         <div class="col-sm-4">
           <div class="card1">
-            <img src="img/item_img.png" style="width: 100%">
-            <h4>タイトル</h4>
-            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
-            <h4 class="cost">20,000円</h4>
+            <img src="img/<?php echo $feed['img_name']; ?>" style="width: 100%">
+            <h4><?php echo $feed['title']; ?></h4>
+            <p><?php echo $feed['feed']; ?></p>
+            <a href="like.php?feed_id=<?php echo $feed["id"]; ?>">
+                      <button class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</butt
+                        on>
+            </a>
+            <h4 class="cost"><?php echo $feed['value']; ?></h4>
+            <span class="like_count">いいね数 : <?php echo $feed["like_cnt"]; ?></span>
           </div>
         </div>
+        <?php } ?>
 
-        <div class="col-sm-4">
-          <div class="card2">
-            <img src="img/item_img.png" style="width: 100%">
-            <h4>タイトル</h4>
-            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
-            <h4 class="cost">20,000円</h4>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div class="card3">
-            <img src="img/item_img.png" style="width: 100%">
-            <h4>タイトル</h4>
-            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
-            <h4 class="cost">20,000円</h4>
-          </div>
-        </div>
+        
         
       </div>
 
@@ -90,38 +108,6 @@
         </div>
         
       </div>
-
-
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="card1">
-            <img src="img/item_img.png" style="width: 100%">
-            <h4>タイトル</h4>
-            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
-            <h4 class="cost">20,000円</h4>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div class="card2">
-            <img src="img/item_img.png" style="width: 100%">
-            <h4>タイトル</h4>
-            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
-            <h4 class="cost">20,000円</h4>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div class="card3">
-            <img src="img/item_img.png" style="width: 100%">
-            <h4>タイトル</h4>
-            <p>ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。ここにストーリが入ります。</p>
-            <h4 class="cost">20,000円</h4>
-          </div>
-        </div>
-        
-      </div>
-
   </div>
 
 
