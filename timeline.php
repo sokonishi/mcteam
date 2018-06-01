34c88f41287a6d6dcbc9e71d67135c9117748b62
小西壮 <>
2018-06-01 11:08:06 +0800

    timeline.php表示できるようになる

--- timeline.php
+++ timeline.php
@@ -63,13 +63,13 @@ $sql = 'SELECT * FROM `feeds` ORDER BY `id` DESC';
       <div class="row">
         <div class="col-sm-4">
           <div class="card<?php echo $feed_cnt; ?>">
-            <img src="img/<?php echo $feed['img_name']; ?>" style="width: 100%">
+            <img src="user_profile_img/<?php echo $feed['img_name']; ?>" style="width: 100%">
             <h4><?php echo $feed['title']; ?></h4>
             <p><?php echo $feed['feed']; ?></p>
             <a href="like.php?feed_id=<?php echo $feed["id"]; ?>">
                       <button class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button>
             </a>
-            <h4 class="cost"><?php echo $feed['value']; ?></h4>
+            <h4 class="cost"><?php echo $feed['price']; ?></h4>
             <span class="like_count">いいね数 : <?php echo $feed["like_cnt"]; ?></span>
           </div>
         </div>
