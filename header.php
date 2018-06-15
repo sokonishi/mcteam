4238626a15f829a45896853b56a909ce898769cf
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-06-15 14:44:33 +0800

    header類

--- header.php
+++ header.php
@@ -1,32 +1,35 @@
- <header>
+<header>
+  <div class="container">
+    <div class="row">
+      <a class="navbar-brand" href="timeline.php"><img src="img/missyou_logo.png" style="width: 20vw"></a>
 
-    
-
-<a class="navbar-brand" href="timeline.php"><img src="img/missyou_logo.png" style="width: 20vw"></a>
-
- 
-    <div class="hamburger-menu type-9">
+      <div class="hamburger-menu type-9">
         <div class="hamburger-menu1"></div>
         <div class="hamburger-menu2"></div>
         <div class="hamburger-menu3"></div>
-    </div>
-    <div class="right-header" >
-      <ul>
-        <li><a href="timeline.php">ホーム</a></li>
-        <li><a href="mypage.php">マイページ</a></li>
-        <li><a href="post.php">投稿</a></li>
-        <li><a href="ranking.php">ランキング</a></li>
-        <li><a href="signout.php">サインアウト</a></li>
-      </ul>
-    </div>
-    
- </header>
-       <div class="header-menu" >
+      </div><!-- /hamburger-menu -->
+      <div class="right-header" >
         <ul>
-          <li><a href="timeline.php">ホーム</a></li>
-          <li><a href="mypage.php">マイページ</a></li>
+<!--           <li><a href="timeline.php">ホーム</a></li>
+          <li><a href="mypage.php">マイページ</a></li> -->
           <li><a href="post.php">投稿</a></li>
-          <li><a href="ranking.php">ランキング</a></li>
+<!--           <li><a href="ranking.php">ランキング</a></li> -->
           <li><a href="signout.php">サインアウト</a></li>
         </ul>
-      </div>
+      </div><!-- /right-header -->
+    </div><!-- /row -->
+  </div><!-- /container -->
+</header>
+  <div class="container">
+    <div class="row">
+      <div class="header-menu" >
+        <ul>
+<!--           <li><a href="timeline.php">ホーム</a></li>
+          <li><a href="mypage.php">マイページ</a></li> -->
+          <li><a href="post.php">投稿</a></li>
+<!--           <li><a href="ranking.php">ランキング</a></li> -->
+          <li><a href="signout.php">サインアウト</a></li>
+        </ul>
+      </div><!-- /header-menu -->
+    </div><!-- /row -->
+  </div><!-- /container -->
